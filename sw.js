4e22dae969e1478f660adf6c18aac50ac2756b4c
Robert <krstb-90@yahoo.com>
2026-08-25 22:14:02 +0200

    Update CACHE_NAME to 'timer-v2' for updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'timer-v1'; // Ändere v2 um Updates zu erzwingen
+const CACHE_NAME = 'timer-v2'; // Ändere v2 um Updates zu erzwingen
 const ASSETS = [
   'index.html',
   'manifest.json',
